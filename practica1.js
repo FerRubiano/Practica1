3c573b528170040b9de4ff20ec94a364ccd29f2a
FerRubiano <millos012@gmail.com>
2021-09-21 21:02:05 -0500

    primer commit

---
+++ practica1.js
@@ -0,0 +1,108 @@
+let nombre = prompt("Ingresa nombre");
+
+const departamentos = () => {
+  let = departamento = prompt(
+    " Ingresa nombre de Departamento iniciando con mayúscula");
+switch (departamento) {
+  case "Amazonas":
+    alert("La capital del departamento es Leticia");
+    break;
+  case "Antioquia":
+    alert("La capital del departamento es Medellín");
+    break;
+  case "Arauca":
+    alert("La capital del departamento es Arauca");
+    break;
+  case "Atlántico":
+    alert("La capital del departamento es Barranquilla");
+    break;
+  case "Bolívar":
+    alert("La capital del departamento es Cartagena de Indias");
+    break;
+  case "Boyacá":
+    alert("La capital del departamento es Tunja");
+    break;
+  case "Caldas":
+    alert("La capital del departamento es Manizales");
+    break;
+  case "Caquetá":
+    alert("La capital del departamento es Florencia");
+    break;
+  case "Casanare":
+    alert("La capital del departamento es Yopal");
+    break;
+  case "Cauca":
+    alert("La capital del departamento es Popayán");
+    break;
+  case "Cesar":
+    alert("La capital del departamento es Valledupar");
+    break;
+  case "Chocó":
+    alert("La capital del departamento es Quibdó");
+    break;
+  case "Córdoba":
+    alert("La capital del departamento es Montería");
+    break;
+  case "Cundinamarca":
+    alert("La capital del departamento es Bogotá D.C");
+    break;
+  case "Guainía":
+    alert("La capital del departamento es Inírida");
+    break;
+  case "Guaviare":
+    alert("La capital del departamento es San José del Guaviare");
+    break;
+  case "Huila":
+    alert("La capital del departamento es Neiva");
+    break;
+  case "La Guajira":
+    alert("La capital del departamento es Riohacha");
+    break;
+  case "Magdalena":
+    alert("La capital del departamento es Santa Marta");
+    break;
+  case "Meta":
+    alert("La capital del departamento es Villavicencio");
+    break;
+  case "Nariño":
+    alert("La capital del departamento es San Juan de Pasto");
+    break;
+  case "Norte de Santander":
+    alert("La capital del departamento es San José de Cúcuta");
+    break;
+  case "Putumayo":
+    alert("La capital del departamento es Mocoa");
+    break;
+  case "Quindío":
+    alert("La capital del departamento es Armenia");
+    break;
+  case "Risaralda":
+    alert("La capital del departamento es Pereira");
+    break;
+  case "San Andrés y Providencia":
+    alert("La capital del departamento es San Andrés");
+    break;
+  case "Santander":
+    alert("La capital del departamento es Bucaramanga");
+    break;
+  case "Sucre":
+    alert("La capital del departamento es Sincelejo");
+    break;
+  case "Tolima":
+    alert("La capital del departamento es Ibagué");
+    break;
+  case "Valle del Cauca":
+    alert("La capital del departamento es Cali");
+    break;
+  case "Vaupés":
+    alert("La capital del departamento es Mitú");
+    break;
+  case "Vichada":
+    alert("La capital del departamento es Puerto Carreño");
+    break;
+
+  default:
+    alert("Por favor ingresa un Departamento valido");
+    break;
+}
+};
